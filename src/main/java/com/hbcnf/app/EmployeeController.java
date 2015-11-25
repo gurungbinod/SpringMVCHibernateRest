63210467d6c0afb7607fa10a4583b4d81f428652
tamubinod <tamu.bnod1@gmail.com>
2015-11-24 18:20:32 -0600

    Added loggers in EmployeeController

--- src/main/java/com/hbcnf/app/EmployeeController.java
+++ src/main/java/com/hbcnf/app/EmployeeController.java
@@ -1,15 +1,13 @@
 package com.hbcnf.app;
 
-import java.util.ArrayList;
 import java.util.List;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -23,24 +21,30 @@ public class EmployeeController {
 	@Autowired
 	private EmployeeService es;
 	
+	private static final Logger logger = LoggerFactory.getLogger(EmployeeController.class);
+	
 	@RequestMapping( "/list" )
 	public List<Employee> getList() {
+		logger.info("Getting list of Employees");
 		return es.findAll();
 	}
 	
 	@RequestMapping( "/list/{fname}" ) 
 	public List<Employee> getListByFirstName (@PathVariable("fname") String fname) {
+		logger.info("Getting list of Employees by Name");
 		return es.findByFirstName(fname);
 	}
 	
 	@RequestMapping( "/{id}" )
 	public Employee getEmployeeById(@PathVariable("id") int id) {
+		logger.info("Getting list of Employees by Id");
 		return es.findById(id);
 	}
 	
 	@RequestMapping( value = "/save", method = RequestMethod.POST )
 	@ResponseBody
 	public Employee saveEmployee(Employee e) {
+		logger.info("Saving Employee to database "+ e.toString());
 		es.addEmployee(e);
 		return e;
 	}
